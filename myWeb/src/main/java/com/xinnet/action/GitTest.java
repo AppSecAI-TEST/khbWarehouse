69950a9207289b7f6dbc8b9b2415edbe1f371217
hongbinkang <2469016045@qq.com>
2017-02-17 17:40:35 +0800

    测试git2

--- myWeb/src/main/java/com/xinnet/action/GitTest.java
+++ myWeb/src/main/java/com/xinnet/action/GitTest.java
@@ -1,9 +1,13 @@
 package com.xinnet.action;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.xinnet.annotation.NotLogin;
+import com.xinnet.utils.DateUtil;
 
 @Controller
 @RequestMapping("git")
@@ -14,5 +18,13 @@ public class GitTest extends BaseAction{
 	public String test() {
 		
 		return jsp("git/testGit");
+	}
+	
+	@RequestMapping("testIP")
+	@NotLogin
+	public String testIP(ModelMap modelMap) {
+		modelMap.put("time",DateUtil.format(new Date(),DateUtil.YYYYMMDDHHMMSS));
+		modelMap.put("ip", request.getRemoteAddr());
+		return jsp("git/testIP");
 	}
 }
