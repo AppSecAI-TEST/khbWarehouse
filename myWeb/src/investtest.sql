92ff4746daac3e1ee18b81c5bf3de1360eb96488
hongbinkang <2469016045@qq.com>
2017-04-08 12:51:29 +0800

    提交sql文件

---
+++ myWeb/src/investtest.sql
@@ -0,0 +1,93 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : khbmysql
+Source Server Version : 50173
+Source Host           : 103.42.29.142:3306
+Source Database       : investtest
+
+Target Server Type    : MYSQL
+Target Server Version : 50173
+File Encoding         : 65001
+
+Date: 2017-04-08 12:50:43
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for class
+-- ----------------------------
+DROP TABLE IF EXISTS `class`;
+CREATE TABLE `class` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `AVAILABLE` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `C_ID` int(11) DEFAULT NULL,
+  `CLASS_NAME` varchar(255) DEFAULT NULL,
+  `AVAILABLE` int(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_identifying_code
+-- ----------------------------
+DROP TABLE IF EXISTS `t_identifying_code`;
+CREATE TABLE `t_identifying_code` (
+  `id` int(10) NOT NULL,
+  `mode` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `creat_time` datetime DEFAULT NULL,
+  `effective` varchar(255) DEFAULT NULL,
+  `effective_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
+  `id` int(10) NOT NULL,
+  `water_num` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `amout` decimal(10,4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `pass_word` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `creat_time` datetime DEFAULT NULL,
+  `head_portrait` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(50) NOT NULL,
+  `password` char(32) NOT NULL,
+  `gender` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
