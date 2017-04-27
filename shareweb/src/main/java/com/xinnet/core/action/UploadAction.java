32849c364564f379873a34812c42305f15d2e255
hongbinkang <2469016045@qq.com>
2017-04-27 11:02:40 +0800

    小修改

--- shareweb/src/main/java/com/xinnet/core/action/UploadAction.java
+++ shareweb/src/main/java/com/xinnet/core/action/UploadAction.java
@@ -10,14 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONObject;
 
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
