1a25f17fff10fc94d6ca6c8e424db4d2560dd702
hongbinkang <2469016045@qq.com>
2017-05-02 16:00:28 +0800

    list根据字段sort排序

--- myWeb/src/test/java/test/ListSortUtils.java
+++ myWeb/src/test/java/test/ListSortUtils.java
@@ -1,4 +1,4 @@
-package com.xinnet.utils;
+package test;
 
 import java.lang.reflect.Field;
 import java.text.NumberFormat;
@@ -10,6 +10,10 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
+
+
   
 /** 
  * 
